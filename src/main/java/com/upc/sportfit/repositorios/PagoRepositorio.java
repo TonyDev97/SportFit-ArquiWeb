35f02879f26c8de6d991637e5fbbe1694b3761c9
Adrian <amallqui38@gmail.com>
2026-09-16 12:59:40 -0500

    Revert "Creacion de Controllers, Services y Repositorys (Pago,Notificacion)"

--- src/main/java/com/upc/sportfit/repositorios/PagoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.upc.sportfit.repositorios;
-
-import com.upc.sportfit.entidades.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PagoRepositorio extends JpaRepository<Pago, Long> {
-}
