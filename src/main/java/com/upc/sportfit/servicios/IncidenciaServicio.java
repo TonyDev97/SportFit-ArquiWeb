9d0ec07194aae9c51de47467a7665e7a02be40a2
nicolecasti <nicolecastillon24@gmail.com>
2026-09-16 13:01:49 -0500

    Incidencias - Nicole

---
+++ src/main/java/com/upc/sportfit/servicios/IncidenciaServicio.java
@@ -0,0 +1,31 @@
+package com.upc.sportfit.servicios;
+
+import com.upc.sportfit.entidades.Incidencia;
+import com.upc.sportfit.repositorios.IncidenciaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IncidenciaServicio {
+    @Autowired
+    private IncidenciaRepositorio incidenciaRepositorio;
+
+    public Incidencia InsertarIncidencia(Incidencia incidencia){
+        return incidenciaRepositorio.save(incidencia);
+    }
+    public List<Incidencia> listarIncidencias(){
+        return incidenciaRepositorio.findAll();
+    }
+    public Incidencia BuscarIncidenciaPorId(Integer id){
+        return incidenciaRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error: Incidencia no encontrada con ID " + id));
+    }
+    public List<Incidencia> listarIncidenciasPorTipo(String tipo){
+        return incidenciaRepositorio.findByTipo(tipo);
+    }
+    public List<Incidencia> listarIncidenciasPorEstado(String estado){
+        return incidenciaRepositorio.findByEstado(estado);
+    }
+}
