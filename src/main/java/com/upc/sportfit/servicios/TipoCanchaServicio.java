1062aea5b410dedccf29d5ffc572d34df238150b
nicolecasti <nicolecastillon24@gmail.com>
2026-09-16 20:21:42 -0500

    Tipo Cancha avance - Nicole

--- src/main/java/com/upc/sportfit/servicios/TipoCanchaServicio.java
+++ src/main/java/com/upc/sportfit/servicios/TipoCanchaServicio.java
@@ -10,38 +10,29 @@ import java.util.List;
 @Service
 public class TipoCanchaServicio {
     @Autowired
-    private TipoCanchaRepositorio canchaRepositorio;
+    private TipoCanchaRepositorio tipoCanchaRepositorio;
 
-    public TipoCancha insertar(TipoCancha tipoCancha) { return canchaRepositorio.save(tipoCancha);}
-
-    public List<TipoCancha> listar(){ return canchaRepositorio.findAll();}
-
-    public TipoCancha actualizar (Integer id , TipoCancha datosActualizados){
-        // 1. Buscamos si existe en la BD
-        TipoCancha canchaExistente = canchaRepositorio.findById(id)
-                .orElseThrow(() -> new RuntimeException("Error: Cancha no encontrada con ID " + id));
-
-        // 2. Modificamos los datos permitidos usando los Setters de Lombok
-        canchaExistente.setNombre(datosActualizados.getNombre());
-        canchaExistente.setAforo(datosActualizados.getAforo());
-        // canchaExistente.setPrecio(datosActualizados.getPrecio()); // Descomenta cuando agregues el precio
-
-        // 3. Guardamos los cambios
-        return canchaRepositorio.save(canchaExistente);
+    public TipoCancha insertar(TipoCancha tipoCancha) {
+        return tipoCanchaRepositorio.save(tipoCancha);
     }
-
+    public List<TipoCancha> listar(){
+        return tipoCanchaRepositorio.findAll();
+    }
+    public TipoCancha actualizar(TipoCancha tipoCancha) {
+        if (tipoCanchaRepositorio.existsById(tipoCancha.getId())) {
+            return tipoCanchaRepositorio.save(tipoCancha);
+        }
+        return null;
+    }
     public void eliminar(Integer id) {
-        // ELIMINACIÓN FÍSICA TEMPORAL PARA PRUEBAS
-        canchaRepositorio.deleteById(id);
-
-        /*
-         * LÓGICA FUTURA (Obligatoria para la evaluación del RF05):
-         * El requerimiento RF05 exige que la desactivación sea una baja lógica[cite: 1].
-         *
-         * TipoCancha cancha = canchaRepositorio.findById(id).orElseThrow();
-         * cancha.setEstado("Inactivo");
-         * canchaRepositorio.save(cancha);
-         */
+        tipoCanchaRepositorio.deleteById(id);
+    }
+    public TipoCancha buscarPorId(Integer id) {
+        return tipoCanchaRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error: Tipo de cancha no encontrada con ID " + id));
+    }
+    public List<TipoCancha> buscarPorDeporte(String deporte) {
+        return tipoCanchaRepositorio.findByDeporte(deporte);
     }
 
 }
